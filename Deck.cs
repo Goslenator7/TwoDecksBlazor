345b803e6843020a7deac66e4b393f0285c60e07
ryan <ryancumming7@gmail.com>
2022-10-10 17:25:15 +0100

    Added Deck class and constructor and reset methods + random generation

---
+++ Deck.cs
@@ -0,0 +1,26 @@
+﻿namespace TwoDecksBlazor
+{
+    public class Deck : List<Card>
+    {
+        private readonly Random random = new Random();
+
+        public Deck()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            this.Clear();
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 0; value <= 13; value++)
+                {
+                    this.Add(new Card((Values)value, (Suits)suit));
+                }
+            }
+        }
+
+
+    }
+}
